1321a3c816a4c07d8139ce392925d8ba8c73f1ce
ziaurjoy <ziaurjoy802@gmail.com>
2021-09-11 14:46:23 +0600

    Serializers

---
+++ django_rest_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from django_rest_app.models import Snippet
+# Register your models here.
+
+admin.site.register(Snippet)
---
+++ django_rest_app/serializers.py
@@ -0,0 +1,39 @@
+
+
+from django.db import models
+from rest_framework import serializers
+from django_rest_app.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+# Manually Create Serializers
+
+
+"""
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        return Snippet.objects.create(self, validated_data)
+
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
+
+"""
+
+# ModelSerializer
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = '__all__'
